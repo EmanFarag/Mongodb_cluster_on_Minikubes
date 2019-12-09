47ac22d36b240de53d7bf8f8700e48fd82e82d66
e.abdelaziz <eman.abdelaziz@westwing.de>
2019-12-09 15:42:44 +0100

    Update pipline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ pipeline {
         stage('build') {
             steps {
                 echo 'Building...'
-                sh '/Users/e.abdelaziz/IdeaProjects/Mongodb_cluster_on_Kubernetes/scripts/generate'
+                sh '/Users/e.abdelaziz/IdeaProjects/Mongodb_cluster_on_Minikubes/up_mongodb_minikube'
                 echo 'Mongodb built successfully'
             }
         }
