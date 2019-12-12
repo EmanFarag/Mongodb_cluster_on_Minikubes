b28f2e1581daf1d247432e1c6dc68a208c6ac16f
e.abdelaziz <eman.abdelaziz@westwing.de>
2019-12-12 14:26:19 +0100

    Update pipline

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,6 +2,7 @@
 ##
 # Script to deploy a Kubernetes project with a StatefulSet running a MongoDB Replica Set, to a local Minikube environment.
 ##
+echo "Keep running the following command until all 'mongod-n' pods are shown as running:  kubectl get all"
 
 # Create keyfile for the MongoD cluster as a Kubernetes shared secret
 TMPFILE=$(mktemp)
@@ -10,12 +11,11 @@ kubectl create secret generic shared-bootstrap-data --from-file=internal-auth-mo
 rm $TMPFILE
 
 # Create mongodb service with mongod stateful-set
-# TODO: Temporarily added no-valudate due to k8s 1.8 bug: https://github.com/kubernetes/kubernetes/issues/53309
 kubectl apply -f ../resources/mongodb-service.yaml --validate=false
-sleep 5
+
 
 # Print current deployment state (unlikely to be finished yet)
-kubectl get all 
+kubectl get all
 kubectl get persistentvolumes
 echo
 echo "Keep running the following command until all 'mongod-n' pods are shown as running:  kubectl get all"
